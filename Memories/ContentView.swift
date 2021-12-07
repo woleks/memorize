daf5bc5d636eb8450bab49cd6d99914f798838d6
Oleksii Vilchanskyi <oleksii.vilchanskyi@strix.net>
2021-12-07 16:16:14 +0100

    Initial Commit

---
+++ Memories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memories
+//
+//  Created by Oleksii Vilchanskyi on 07/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Oleksii Vilchanskyi on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
